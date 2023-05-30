5435570302415689f210c4619d98c4f4cbc65752
franciscossilva <francisco.dossantos@zup.com.br>
2023-05-30 09:41:33 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import  java.util.Scanner;
+public class Numerosdecemaum {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
